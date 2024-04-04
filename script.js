f6f11515312818f7cdd22927ba06e5587de94ca8
RuchiF <ruchif26@gmail.com>
2024-04-04 17:26:46 +0530

    script.js was added

---
+++ script.js
@@ -0,0 +1,36 @@
+// Define your API key
+const apiKey = "09e69c8e408f524e3514bb879e83635a";
+const weatherIcon = document.querySelector(".weather-icon");
+// const city = "Gujrat";
+async function checkWeatherDetails(city) {
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  const response = await fetch(url);
+  if (response.status === 404) {
+    document.querySelector(".error").style.display = "block";
+    document.querySelector(".weather").style.display = "none";
+  } else {
+    var data = await response.json();
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temperature").innerHTML = data.main.temp + "°C";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+    if (data.weather[0].main == "Clouds") {
+      weatherIcon.src = "images/clouds.png";
+    } else if (data.weather[0].main == "Clear") {
+      weatherIcon.src = "images/clear.png";
+    } else if (data.weather[0].main == "Rain") {
+      weatherIcon.src = "images/rain.png";
+    } else if (data.weather[0].main == "Drizzle") {
+      weatherIcon.src = "images/drizzle.png";
+    } else if (data.weather[0].main == "Mist") {
+      weatherIcon.src = "images/mist.png";
+    }
+    document.querySelector(".weather").style.display = "block";
+    document.querySelector(".error").style.display = "none";
+  }
+}
+const cityInput = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+searchBtn.addEventListener("click", () => {
+  checkWeatherDetails(cityInput.value);
+});
